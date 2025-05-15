9c966c9f4919355203cc08bdc9ddb038fab6e2db
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2025-05-15 01:05:01 -0500

    update to use elasticsearch

--- sync.py
+++ sync.py
@@ -1,29 +1,21 @@
+from copy import deepcopy
+from datetime import datetime
+import json
 import requests
 from hubspot import Client
 from hubspot.marketing.events import BatchInputMarketingEventCreateRequestParams, ApiException
-from pprint import pprint
 SEASONS_API = 'https://my.firstinspires.org/usfirstapi/seasons/search'
-EVENTS_API = 'https://my.firstinspires.org/usfirstapi/events/search?CountryCode=US&StateProv=AL&Season={season_year}&ProgramCode={program_code}'
+ELASTIC_SEARCH_EVENTS_URL = 'https://es02.firstinspires.org/events/_search'
 FIRST_PROGRAMS = set([ 'JFLL', 'FLL', 'FTC', 'FRC' ])
 
 '''
 Get the current seasons for all FIRST programs
 '''
-def fetch_first_seasons() -> dict: 
+def fetch_first_seasons() -> list: 
   seasons = None
   try: seasons = { s['ProgramCode']: s['SeasonYearStart'] for s in requests.get(SEASONS_API).json() if s['IsCurrentSeason']}
   except: seasons = None
   return seasons
-
-
-'''
-Get the FIRST events for a given program and season
-'''
-def fetch_first_events(program_code: str, season_year: int) -> list:
-  events = None
-  try: events = requests.get(EVENTS_API.format(program_code=program_code, season_year=season_year)).json()
-  except: events = None  
-  return events
 
 
 '''
@@ -108,48 +100,41 @@ def get_custom_property(custom_properties: list, key: str) -> str:
 Create events that are new and update events that already exist.
 Events are unique by a combination of event code, program code, and season year.
 '''
-def process_events(api_client: Client, first_events: list, hubspot_events: list, program_code: str, season_year: int):  
-  # Find the existing HubSpot events that match this batch
-  program_hubspot_events = [
+def process_events(api_client: Client, first_events: list, hubspot_events: list, frc_season_year: int):  
+  # Find the existing HubSpot events that match for the season
+  existing_hubspot_events = [
     event 
     for event 
     in hubspot_events 
-    if event.event_type == program_code 
-    and get_custom_property(event.custom_properties, 'event_season_year') == str(season_year)
+    if (event.event_type == 'FRC' and get_custom_property(event.custom_properties, 'event_season_year') == str(frc_season_year))
+    or (event.event_type != 'FRC' and get_custom_property(event.custom_properties, 'event_season_year') == str(frc_season_year - 1))
   ]
 
   events_to_update = []
 
   # Get events to update
-  for existing_event in program_hubspot_events:
+  for existing_event in existing_hubspot_events:
     match = [
       event 
       for event in first_events 
-      if event['Code'] == get_custom_property(existing_event.custom_properties, 'event_code')
-      and event['Type'] == program_code
-      and event['Season'] == season_year
+      if event['externalEventId'] == existing_event.external_event_id
     ]
 
     if len(match) == 0: continue
     if len(match) > 1:
-      print('Too many matches found for', program_code, first_event['Code'], season_year, 'in existing events')
+      print('Too many matches found for', existing_event.external_event_id, 'in existing events')
       continue
 
-    first_event = match[0]
-    hubspot_event = map_first_event_properties_to_hubspot(first_event)
-    hubspot_event['externalEventId'] = existing_event.external_event_id
-    hubspot_event['objectId'] = existing_event.object_id
-    hubspot_event['eventOrganizer'] = existing_event.event_organizer
+    first_event = deepcopy(match[0])
+    first_event['externalEventId'] = existing_event.external_event_id
+    first_event['objectId'] = existing_event.object_id
+    first_event['eventOrganizer'] = existing_event.event_organizer
     
-    events_to_update.append(hubspot_event)
-    first_events.remove(first_event)
+    events_to_update.append(first_event)
+    first_events.remove(match[0])
 
-  events_to_create = []
-
-  # Create new events
-  for first_event in first_events:
-    hubspot_event =  map_first_event_properties_to_hubspot(first_event)
-    events_to_create.append(hubspot_event)
+  # Create remaining events
+  events_to_create = deepcopy(first_events)
 
   all_events = events_to_update + events_to_create
   if len(all_events) > 0:
@@ -168,6 +153,118 @@ def get_hubspot_api_token() -> str:
     api_token = None
   return api_token
 
+def get_elastic_search_events(frc_season: int) -> dict:
+  queryParameters = { 'size': '200' }
+  headers = {'Content-type': 'application/json'}
+  body = {
+    "query": {
+      "bool": {
+        "must": [
+          {
+            "match": {
+              "countryCode": "US"
+            }
+          },
+          {
+            "match": {
+              "event_stateprov": "AL"
+            }
+          },
+          {
+            "bool": {
+              "should": [
+                {
+                  "bool": {
+                    "must": [
+                      {
+                        "match": {
+                          "event_season": frc_season
+                        }
+                      },
+                      {
+                        "match": {
+                          "event_type": "FRC"
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  "bool": {
+                    "must": [
+                      {
+                        "match": {
+                          "event_season": frc_season - 1
+                        }
+                      },
+                      {
+                        "bool": {
+                          "must_not": [
+                            {
+                              "match": {
+                                "event_type": "FRC"
+                              }
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  response = requests.get(ELASTIC_SEARCH_EVENTS_URL, params=queryParameters, data=json.dumps(body), headers=headers).json()
+  events = [e['_source'] for e in response['hits']['hits']]
+  return [
+    {
+      'eventOrganizer': 'FIRST in Alabama',
+      'externalAccountId': str(e['event_type']) + str(e['event_season']) + str(e['event_code']),
+      'externalEventId': str(e['event_type']) + str(e['event_season']) + str(e['event_code']),
+      'eventName': e['event_name'],
+      'eventType': e['event_type'],
+      'startDateTime': int(datetime.fromisoformat(e['date_start']).timestamp() * 1000),
+      'endDateTime': int(datetime.fromisoformat(e['date_end']).timestamp() * 1000),
+      "customProperties": [
+        {
+          'name': 'event_code',
+          'value': str(e['event_code'])
+        },
+        {
+          'name': 'event_venue',
+          'value': e['event_venue']
+        },
+        {
+          "name": "event_season_year",
+          "value": e['event_season']
+        },
+        {
+            "name": "event_volunteer_url",
+            "value": ''
+        },
+        {
+            "name": "event_city",
+            "value": e['event_city']
+        },
+        {
+            "name": "event_postal_code",
+            "value": e['event_postalcode']
+        },
+        {
+            "name": "event_address",
+            "value": e['event_address1']
+        }
+      ]
+    }
+    for e 
+    in events]
+
+
 def main():
   print('Sync Begin')
 
@@ -184,21 +281,19 @@ def main():
     print('Could not fetch current seasons')
     return
   
-  for program_code, season_year in seasons.items():
-    if program_code not in FIRST_PROGRAMS: continue
-
-    first_events = fetch_first_events(program_code, season_year)
-    if first_events is None:
-      print('Unable to find events for', program_code)
-      continue
-    
-    process_events(
-      api_client,
-      first_events, 
-      hubspot_events,
-      program_code,
-      season_year
-    )
+  frc_season = seasons['FRC']
+  first_events = get_elastic_search_events(frc_season)
+  if first_events is None:
+    print('Unable to find events for the', frc_season, 'FIRST season')
+    return
+  
+  process_events(
+    api_client,
+    first_events, 
+    hubspot_events,
+    frc_season
+  )
+  
   print('Sync Complete')
 
 if __name__ == '__main__':
